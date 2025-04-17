97de12de6f931d781cdc3d6057961c4ddda7a8e4
Gustavo Gutierrez <gustavo2023@gmail.com>
2025-04-17 15:14:18 -0400

    feat: add character set generation and DOM element references for password generation

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
+// DOM elements
 const form = document.getElementById("password-form");
 const slider = document.getElementById("char-input");
 const sliderColorDisplay = document.querySelector(".range-color-display");
 const charLengthSpan = document.getElementById("char-length");
+const passwordInput = document.getElementById("password");
+const copyTextSpan = document.getElementById("copy-span");
+const copyBtn = document.getElementById("copy-btn");
+const upperCaseCheckbox = document.getElementById("uppercase-option");
+const lowerCaseCheckbox = document.getElementById("lowercase-option");
+const numbersCheckbox = document.getElementById("numbers-option");
+const symbolsCheckbox = document.getElementById("symbols-option");
+
+const generateCharArrays = (begin, end) => {
+  const charArray = [];
+
+  for (let i = begin; i <= end; i++) {
+    charArray.push(String.fromCharCode(i));
+  }
+
+  return charArray;
+};
+
+// Character sets
+const LOWERCASE_CHARS = generateCharArrays(97, 122); // a-z
+const UPPERCASE_CHARS = generateCharArrays(65, 90); // A-Z
+const NUMBER_CHARS = generateCharArrays(48, 57); // 0-9
+const SYMBOL_CHARS = generateCharArrays(33, 47).concat(
+  generateCharArrays(58, 64),
+  generateCharArrays(91, 96),
+  generateCharArrays(123, 126)
+);
 
 const updateSliderFill = () => {
   const min = slider.min ? parseFloat(slider.min) : 0;
